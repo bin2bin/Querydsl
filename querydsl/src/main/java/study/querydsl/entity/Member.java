36b65e4bfc51f03309c6bcfde51ec68882e1cc37
LimHyunBin <dlagusqls152@naver.com>
2023-03-09 23:59:24 +0900

    집합, join~

---
+++ querydsl/src/main/java/study/querydsl/entity/Member.java
@@ -0,0 +1,46 @@
+package study.querydsl.entity;
+
+import lombok.*;
+
+import javax.persistence.*;
+
+import static javax.persistence.FetchType.LAZY;
+
+@Entity
+@Getter @Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@ToString(of = {"id", "username", "age"}) // 본인이 소유한 필드 정도만 적어주고 연관관계는 적지 않는게 좋음 서로 왔다갔다 해서 무한루프 돌음
+public class Member {
+
+    @Id @GeneratedValue
+    @Column(name = "member_id")
+    private Long id;
+    private String username;
+    private int age;
+
+    @ManyToOne(fetch = LAZY)
+    @JoinColumn(name = "team_id")
+    private Team team;
+
+    public Member(String username) {
+        this(username, 0);
+    }
+
+    public Member(String username, int age) {
+        this(username, age, null);
+    }
+
+    public Member(String username, int age, Team team) {
+        this.username = username;
+        this.age = age;
+        if(team != null) {
+            changeTeam(team);
+        }
+    }
+    public void changeTeam(Team team) {
+        this.team = team;
+        team.getMembers().add(this);
+    }
+
+
+}
